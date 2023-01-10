23460e9a3680df403085798e81ff029274246eca
agust <agust@AGUS>
2023-01-10 17:05:28 -0300

    Agregar archivos de proyecto.

---
+++ TpGrupal/Aereo/Aereo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpGrupal.Aereo
+{
+    public abstract class Aereo
+    {
+        public Areo(string color, string marca){
+            Color = color;
+            Marca = marca;
+
+     }
+        public string Color { get; set; }
+        public string Marca { get; set; }
+    }
+}
